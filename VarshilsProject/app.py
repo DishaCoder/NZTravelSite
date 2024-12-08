cb91a1889523acf35c98901848898d091e63e17e
Disha Patel <dishapatel@Dishas-MacBook-Air.local>
2024-12-07 20:31:28 -0500

    backend python file content

---
+++ VarshilsProject/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, template_folder='pages', static_folder='style')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
